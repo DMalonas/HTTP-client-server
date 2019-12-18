6467f0d826d90b5a8259c376fcb3f2698e359aaa
DimitriosMalonas <dimitriosmalonas@outlook.com>
2019-12-18 08:03:25 +0000

    Server socket, threadpool, file for logging, run ClientHandler

--- WebServer/src/httpserver/ClientHandler.java
+++ WebServer/src/httpserver/ClientHandler.java
@@ -1,5 +1,12 @@
 package httpserver;
 
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class ClientHandler {
+
+	public ClientHandler(Socket s, PrintWriter out, String workingDirectory) {
+		// TODO Auto-generated constructor stub
+	}
 
 }
--- WebServer/src/httpserver/Server.java
+++ WebServer/src/httpserver/Server.java
@@ -1,9 +1,40 @@
 package httpserver;
 
-public class Server {
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
+
+public class Server {
+    public static final int MAX_NUMBER_OF_THREADS = 2;
+    private int connectionCount = 0;
+
+    private int port;
+    private String workingDirectory;
+   
+    private PrintWriter out;
+
+    
 	public Server(int port, String workingDirectoryString) {
-		// TODO Auto-generated constructor stub
+        this.port = port;
+        try {
+            ServerSocket ss = new ServerSocket(this.port);
+            out = new PrintWriter(new FileWriter("Log.txt", true));
+            this.workingDirectory = workingDirectory;
+            ExecutorService threadPool = Executors.newFixedThreadPool(MAX_NUMBER_OF_THREADS);
+            while (true) {
+                Socket s = ss.accept();
+                //prepare runnable content
+                ClientHandler myClientHandler = new ClientHandler(s, out, this.workingDirectory);
+              //  threadPool.submit(myClientHandler);
+            }	
+    	} catch (IOException e) {
+    		System.out.println("Trouble with connection" + e);
+    	}
 	}
 
 }
